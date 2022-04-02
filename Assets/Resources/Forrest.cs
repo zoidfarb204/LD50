3219d5a502dc88436a8a27759f8c6f2d105b54da
Mike Rosenberger <mrosenberger@sciengineering.com>
2022-04-01 22:20:54 -0500

    Initial work on resources

---
+++ Assets/Resources/Forrest.cs
@@ -0,0 +1,15 @@
+﻿using Enums;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Resources
+{
+    public class Forrest : IResource
+    {
+        public string Name => "Forrest";
+        public TileBase Tile { get; set; }
+        public int Amount { get; set; }
+        public ResourceType Type => ResourceType.Wood;
+        public Vector3Int Location { get; set; }
+    }
+}
